de449ab58b4984a8374c10f5a922dc9a83e52a1a
William Pratt <willpratt78@gmail.com>
2022-03-22 13:50:07 -0700

    Ass css atyling, if/else statement changing text of button
    Co-authored-by: Jimmy Fox <jimmyfox00@gmail.com>
    Co-authored-by: Jeremy Martin <jeremyvs1945@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,41 @@
 $(document).ready(function() {
+  let bool1=false
   $("#button1").click(function() {
+    if(bool1 === false){
+      $("#button1").text("Hide Definition");
+      bool1 = true;
+    } else {
+      $("#button1").text("Show Definition");
+      bool1 = false
+    }
     $("#p1").toggle(".hide");
   });
 });
 
 $(document).ready(function() {
+  let bool2 = false
   $("#button2").click(function() {
+    if(bool2 === false){
+      $("#button2").text("Hide Definition");
+      bool2 = true;
+    } else {
+      $("#button2").text("Show Definition");
+      bool2 = false
+    }
     $("#p2").toggle(".hide");
   });
 });
 
 $(document).ready(function() {
+  let bool3 = false
   $("#button3").click(function() {
+    if(bool3 === false){
+      $("#button3").text("Hide Definition");
+      bool3 = true;
+    } else {
+      $("#button3").text("Show Definition");
+      bool3 = false
+    }
     $("#p3").toggle(".hide");
   });
 });
