f728701374d9bad384167792de41380c524f5ed3
William Pratt <willpratt78@gmail.com>
2022-03-22 13:33:48 -0700

    add html, js, and css to make a flashcard website
    Co-authored-by:[200~
    
    q

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("#button1").click(function() {
+    $("#p1").toggle(".hide");
+  });
+});
+
+$(document).ready(function() {
+  $("#button2").click(function() {
+    $("#p2").toggle(".hide");
+  });
+});
+
+$(document).ready(function() {
+  $("#button3").click(function() {
+    $("#p3").toggle(".hide");
+  });
+});
+
+$(document).ready(function() {
+  $("#button4").click(function() {
+    $("#p4").toggle(".hide");
+  });
+});
+
+$(document).ready(function() {
+  $("#button5").click(function() {
+    $("#p5").toggle(".hide");
+  });
+});
